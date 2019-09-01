8e79d776c799ac2b1805e75ce085968264e6a7de
merlox <merunasgrincalaitis@gmail.com>
2019-09-01 13:59:25 +0200

    Added yargs npm scripts

--- server.js
+++ server.js
@@ -9,8 +9,19 @@ const fs = require('fs')
 const bodyParser = require('body-parser')
 const path = require('path')
 const Store = require('data-store')
-const port = 80
 const storage = new Store({path: 'config.json'})
+const yargs = require('yargs')
+const argv = yargs.option('port', {
+    alias: 'p',
+    description: 'Set the port to run this server on',
+    type: 'number',
+}).help().alias('help', 'h').argv
+if(!argv.port) {
+    console.log('Error, you need to pass the port you want to run this application on with npm start -- -p 8001')
+    process.exit(0)
+}
+const port = argv.port
+
 let infura
 let contractAddress
 let contractInstance
@@ -31,9 +42,11 @@ if(process.env.NODE_ENV == 'development') {
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
-app.use((req, res, next) => {
-	console.log(`Requesting ${req.url}`)
-    next()
+app.use('*', (req, res, next) => {
+	// Logger
+	let time = new Date()
+	console.log(`${req.method} to ${req.originalUrl} at ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`)
+	next()
 })
 app.post('/seed-phrase', (req, res) => {
 	let mnemonic = req.body.seedPhrase.trim()
